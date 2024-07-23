5676695e73c37281dca4f3fdf1b49dc51644a315
bfueu <jijjw462@gmail.com>
2024-07-23 14:43:35 +0530

    JAVA PROGRAM

---
+++ mk.java
@@ -0,0 +1,13 @@
+class sum
+{
+public static void main(String args[])
+{
+int[]array = {23,28,65,98,87,54};
+for(int i=0;i<array.length;i++)
+{
+System.out.println("index "+i+" "+array[i]);
+}
+}
+}
+
+
